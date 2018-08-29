056f9cadde33e7851824bac8a23959804e07c03b
athlosproject <albert.sanchez@pssjd.org>
2018-08-29 17:19:34 +0200

    version 1.0.1

---
+++ _DS_Rmd/memoryDS.R
@@ -0,0 +1,7 @@
+## ---- global
+
+miss_values_vector <- c(991, 992, 993, 994, 995, 996, 997, 998, 999)
+ds_label <- "Difficulties with memory"
+cat_label <- c("Yes" = 0, "No" = 1, "Missing" = 999, "Do not know" = 998, "Refuse" = 997, "Not applicable" = 996, "Does not answer" = 995, "Not attempt not done" = 994, "Disable to measure" = 993, "Impute" = 992, "CAPI interviewer error" = 991)
+datafolder <- "../RData/"
+hd_vbl <- "memory"
